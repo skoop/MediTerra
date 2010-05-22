399ac1ee531ba744adf7b3abebf2c986ef24df72
Stefan Koopmanschap <stefan.koopmanschap@gmail.com>
2010-05-22 14:48:41 +0200

    added new options and changed existing options

--- config/config.php
+++ config/config.php
@@ -1,8 +1,10 @@
 <?php
 // configuration will be placed here
 $config = array(
-  'template'        => 'default',
-  'sqlazure_server' => '',
-  'sqlazure_user'   => '',
-  'sqlazure_pass'   => '',
+  'template'              => 'default',
+  'sqlazure_table_server' => '',
+  'sqlazure_blob_server'  => '',
+  'sqlazure_queue_server' => '',
+  'sqlazure_user'         => '',
+  'sqlazure_pass'         => '',
 );
