57a6bb7d614a0b79e37ebbee59ac2da5cd48c062
Stefan Koopmanschap <stefan.koopmanschap@gmail.com>
2010-05-22 14:47:34 +0200

    basic blob container logic implemented: list and create now works

--- controllers/BlobController.php
+++ controllers/BlobController.php
@@ -1,19 +1,70 @@
 <?php
 
+require_once 'Microsoft/WindowsAzure/Storage/Blob.php';
+
 /**
  * Controller for the Azure Blob Storage
  */
-class BlobController implements ControllerInterface
+class BlobController extends BaseController implements ControllerInterface
 {
   /**
-   * Triggers the execution of the blob controller
+   * Initial page of the Blob controller, lists the available Blob containers
    *
    * @param sfEventDispatcher $dispatcher
    * @param sfWebRequest $request
+   * @param array $config
    * @return string
    */
-  public function execute(sfEventDispatcher $dispatcher, sfWebRequest $request)
+  public function executeIndex(sfEventDispatcher $dispatcher, sfWebRequest $request, $config)
   {
-    
+    $blob_object = $this->getBlobObject($config);
+    $twig = $this->getTwig($config, 'blob');
+    $template = $twig->loadTemplate('container-list.tmpl');
+
+    $blob_containers = $blob_object->listContainers();
+    $containers = array();
+    foreach($blob_containers as $container)
+    {
+      $containers[] = array('name' => $container->Name);
+    }
+
+    return $template->render(array('containercount' => count($containers), 'containers' => $containers));
+  }
+
+  public function executeCreatecontainer(sfEventDispatcher $dispatcher, sfWebRequest $request, $config)
+  {
+    $blob_object = $this->getBlobObject($config);
+    $twig = $this->getTwig($config, 'blob');
+
+    if ($request->getMethod() == sfWebRequest::POST)
+    {
+
+      if ($blob_object->containerExists($request->getParameter('containername')))
+      {
+        $template = $twig->loadTemplate('error.tmpl');
+        return $template->render(array('error' => 'A container named '.$request->getParameter('containername').' already exists'));
+      }
+      $template = $twig->loadTemplate('create-container-success.tmpl');
+
+      $blob_object->createContainer($request->getParameter('containername'));
+      
+      return $template->render(array('containername' => $request->getParameter('containername')));
+    }
+    else
+    {
+      $template = $twig->loadTemplate('create-container.tmpl');
+
+      return $template->render(array());
+    }
+  }
+
+  /**
+   * 
+   * @param  $config
+   * @return Microsoft_WindowsAzure_Storage_Blob
+   */
+  protected function getBlobObject($config)
+  {
+    return new Microsoft_WindowsAzure_Storage_Blob($config['sqlazure_blob_server'], $config['sqlazure_user'], $config['sqlazure_pass']);
   }
 }
