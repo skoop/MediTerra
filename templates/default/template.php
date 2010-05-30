29703d0473d283453f110d99fece5cb0280a63f0
Stefan Koopmanschap <stefan.koopmanschap@gmail.com>
2010-05-30 22:32:01 +0200

    adding some style and the logo designed by marjolein van elteren

--- templates/default/template.php
+++ templates/default/template.php
@@ -1,9 +1,10 @@
 <html>
 <head>
 <title>MediTerra</title>
+  <link type="text/css" rel="stylesheet" href="/css/style.css" />
 </head>
 <body>
-<h1><a href="/">MediTerra</a></h1>
+<h1><a href="/"><img src="/images/mediterra.png" alt="MediTerra" /></a></h1>
 <div id="nav"><a href="/?controller=BlobController">Blobs</a> || <a href="/?controller=TableController">Tables</a> || <a href="/?controller=QueueController">Queues</a></div>
 {{CONTENT}}
 </body>
