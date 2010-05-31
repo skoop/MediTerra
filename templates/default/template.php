c2d67ac050497e5e04e09b90e217b287cbc82f7a
Stefan Koopmanschap <stefan.koopmanschap@gmail.com>
2010-05-31 21:33:33 +0200

    removed queue option for now

--- templates/default/template.php
+++ templates/default/template.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 <h1><a href="/"><img src="/images/mediterra.png" alt="MediTerra" /></a></h1>
-<div id="nav"><a href="/?controller=BlobController">Blobs</a> || <a href="/?controller=TableController">Tables</a> || <a href="/?controller=QueueController">Queues</a></div>
+<div id="nav"><a href="/?controller=BlobController">Blobs</a> || <a href="/?controller=TableController">Tables</a><!-- || <a href="/?controller=QueueController">Queues</a>--></div>
 {{CONTENT}}
 </body>
 </html>
