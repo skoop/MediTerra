ecc551c80f79ebdb7e82cab36167e491c35bb948
Stefan Koopmanschap <left@leftontheweb.com>
2010-07-14 23:26:03 +0200

    added debug option

--- config/config.php
+++ config/config.php
@@ -1,8 +1,11 @@
 <?php
 // configuration will be placed here
 $config = array(
-  'template'        => 'default',
-  'sqlazure_server' => '',
-  'sqlazure_user'   => '',
-  'sqlazure_pass'   => '',
+  'template'              => 'default',
+  'sqlazure_table_server' => 'table.core.windows.net',
+  'sqlazure_blob_server'  => 'blob.core.windows.net',
+  'sqlazure_queue_server' => 'queue.core.windows.net',
+  'sqlazure_user'         => '',
+  'sqlazure_pass'         => '',
+  'debug'				          => false,
 );
--- htdocs/index.php
+++ htdocs/index.php
@@ -8,39 +8,52 @@ set_include_path(dirname(__FILE__).'/../lib'.PATH_SEPARATOR.dirname(__FILE__).'/
 // get the configuration
 require_once(dirname(__FILE__).'/../config/config.php');
 
-// include classes that are always used
-require_once('Symfony/sfException.class.php');
-require_once('Symfony/event_dispatcher/sfEvent.php');
-require_once('Symfony/event_dispatcher/sfEventDispatcher.php');
-require_once('Symfony/sfParameterHolder.class.php');
-require_once('Symfony/sfToolkit.class.php');
-require_once('Symfony/request/sfRequest.class.php');
-require_once('Symfony/request/sfWebRequest.class.php');
-require_once('Symfony/response/sfResponse.class.php');
-require_once('Symfony/response/sfWebResponse.class.php');
-require_once('Twig/Autoloader.php');
-require_once('interfaces/ControllerInterface.php');
-require_once('controllers/BaseController.php');
-require_once('output/MediTerraOutput.php');
+try {
 
-// initialize Twig autoloader
-Twig_Autoloader::register();
+  // include classes that are always used
+  require_once('Symfony/sfException.class.php');
+  require_once('Symfony/event_dispatcher/sfEvent.php');
+  require_once('Symfony/event_dispatcher/sfEventDispatcher.php');
+  require_once('Symfony/sfParameterHolder.class.php');
+  require_once('Symfony/sfToolkit.class.php');
+  require_once('Symfony/request/sfRequest.class.php');
+  require_once('Symfony/request/sfWebRequest.class.php');
+  require_once('Symfony/response/sfResponse.class.php');
+  require_once('Symfony/response/sfWebResponse.class.php');
+  require_once('Twig/Autoloader.php');
+  require_once('interfaces/ControllerInterface.php');
+  require_once('controllers/BaseController.php');
+  require_once('output/MediTerraOutput.php');
 
-// initialize dispatcher
-$dispatcher = new sfEventDispatcher();
-// initialize request
-$request = new sfWebRequest($dispatcher);
+  // initialize Twig autoloader
+  Twig_Autoloader::register();
 
-$controller_name = $request->getParameter('controller');
-if (empty($controller_name) || !in_array($controller_name, array('BlobController', 'TableController', 'QueueController')))
-{
-  $controller_name = 'IndexController';
+  // initialize dispatcher
+  $dispatcher = new sfEventDispatcher();
+  // initialize request
+  $request = new sfWebRequest($dispatcher);
+
+  $controller_name = $request->getParameter('controller');
+  if (empty($controller_name) || !in_array($controller_name, array('BlobController', 'TableController', 'QueueController')))
+  {
+    $controller_name = 'IndexController';
+  }
+  require_once(dirname(__FILE__).'/../controllers/'.$controller_name.'.php');
+
+  $controller = new $controller_name();
+  $content = $controller->execute($dispatcher, $request, $config);
 }
-require_once(dirname(__FILE__).'/../controllers/'.$controller_name.'.php');
-
-$controller = new $controller_name();
-$content = $controller->execute($dispatcher, $request, $config);
-
+catch (Exception $e)
+{
+  if ($config['debug'])
+  {
+    throw $e;	
+  }
+  else
+  {
+    $content = $e->getMessage();
+  }
+}
 if (!is_null($content))
 {
   // get the template path
